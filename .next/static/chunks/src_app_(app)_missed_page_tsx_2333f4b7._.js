482a498ff1c96a0a4ae33798f830d2c01b111731
Aryan Chordia <acho0098@student.monash.edu>
2026-06-06 02:48:42 +1000

    chore: ignore .next and remove build artifacts from index

--- .next/static/chunks/src_app_(app)_missed_page_tsx_2333f4b7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_9716c342._.js",
-  "static/chunks/node_modules_62d872b3._.js"
-],
-    source: "dynamic"
-});
